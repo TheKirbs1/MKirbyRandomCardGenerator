5b963844a49a224eafa6e8d9ca790435e3f7c283
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-04-16 01:03:36 +0000

    suits & values added

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = ["♦", "♥", "♠", "♣"];
+  let value = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  function generateRandomSuit() {
+    return suit[Math.floor(Math.random() * suit.length)];
+  }
+  function generateRandomValue() {
+    return value[Math.floor(Math.random() * value.length)];
+  }
+
+  let randomSuit = generateRandomSuit();
+  let randomValue = generateRandomValue();
+  document.querySelector(".card").innerHTML =
+    randomSuit + randomValue + randomSuit;
+
+  if (randomSuit === "♦" || randomSuit === "♥") {
+    document.querySelector(".card").style.color = "red";
+  }
+
+  console.log(randomSuit + randomValue + randomSuit);
 };
